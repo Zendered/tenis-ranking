a2fecf98a4248a9de0e020a55c4d8d311981140c
Luiz <luizz.ggr@gmail.com>
2022-06-01 19:34:35 -0300

    feat: adding players controller/service and prisma service

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { PlayersController } from './players/controllers/players.controller';
+import { PlayersService } from './players/services/players.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [PlayersController],
+  providers: [PrismaService, PlayersService],
 })
 export class HttpModule {}
