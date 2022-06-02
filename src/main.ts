ed8c9cdbcfe7443f211768143ea523502c43a2a5
Luiz <luizz.ggr@gmail.com>
2022-06-02 03:58:14 -0300

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Tennis ranking')
+    .setDescription('Api that ranking players who plays tennis')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
   await app.listen(3001);
 }
 bootstrap();
